c5499a7b7afbe560c582547c2654dc0641289244
Filipe Barbosa <filipe.barbosa@gazin.com.br>
2023-10-05 18:00:28 -0300

    feat: [task-3] adicionando modulo do mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AccountsModule } from './accounts/accounts.module';
+import { TransactionsModule } from './transactions/transactions.module';
+import configuration from './config/configuration';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(`mongodb://localhost:${configuration().mongoDB.port}/test`),
+    AccountsModule,
+    TransactionsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
