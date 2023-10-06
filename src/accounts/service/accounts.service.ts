24f92c84505574adbb7461eba91b93662fa72001
Filipe Barbosa <filipe.barbosa@gazin.com.br>
2023-10-05 23:44:18 -0300

    feat: [task-10] ajuste de identação e tipagem

--- src/accounts/service/accounts.service.ts
+++ src/accounts/service/accounts.service.ts
@@ -1,4 +1,4 @@
-import {HttpStatus, Injectable} from '@nestjs/common';
+import { HttpStatus, Injectable } from '@nestjs/common';
 import { AccountDto } from '../dto/account.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
@@ -8,7 +8,7 @@ import { AccountNotFound, ExistsAccountsException } from '../exception/accounts.
 export class AccountsService {
   constructor(@InjectModel('Account') private readonly accountModel: Model<AccountDto>) {}
 
-  async create(accountId: string, creating: boolean) {
+  async create(accountId: string, creating: boolean): Promise<HttpStatus> {
     const recoveredAccount = await this.findOneByAccountId(accountId, creating);
     if (recoveredAccount || recoveredAccount != null) {
       throw new ExistsAccountsException();
@@ -22,22 +22,22 @@ export class AccountsService {
     try {
       const createdAccount = new this.accountModel(newAccount);
       await createdAccount.save();
-      return HttpStatus.CREATED
-    }catch (e) {
-      throw new Error(e)
+      return HttpStatus.CREATED;
+    } catch (e) {
+      throw new Error(e);
     }
   }
 
-  async findOneByAccountId(accountId: string, creating: boolean) {
+  async findOneByAccountId(accountId: string, creating: boolean): Promise<{} | AccountDto> {
     const recoveredAccount = await this.accountModel.findOne({ accountId }).exec();
-    if(!recoveredAccount && creating){
-      return recoveredAccount
+    if (!recoveredAccount && creating) {
+      return recoveredAccount;
     }
 
     if (recoveredAccount && !creating) {
       return this.mountWantedRecoveredData(recoveredAccount);
     }
-    throw new AccountNotFound
+    throw new AccountNotFound();
   }
 
   mountWantedRecoveredData(recoveredAccount: AccountDto) {
