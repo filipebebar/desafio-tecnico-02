e95121135e65205ac1b95270e0ab7b5ee2d74fdf
Filipe Barbosa <filipe.barbosa@gazin.com.br>
2023-10-06 14:00:53 -0300

    feat: [task-17] implementação de testes de controller

--- src/accounts/controller/accounts.controller.spec.ts
+++ src/accounts/controller/accounts.controller.spec.ts
@@ -1,20 +1,68 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AccountsController } from './accounts.controller';
 import { AccountsService } from '../service/accounts.service';
+import { HttpStatus } from '@nestjs/common';
+import { getModelToken } from '@nestjs/mongoose';
 
 describe('AccountsController', () => {
-  let controller: AccountsController;
+  let accountsController: AccountsController;
+  let accountsService: AccountsService;
 
   beforeEach(async () => {
+    const mockAccountModel = {
+      create: jest.fn(),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AccountsController],
-      providers: [AccountsService],
+      providers: [
+        AccountsService,
+        {
+          provide: getModelToken('Account'),
+          useValue: mockAccountModel,
+        },
+      ],
     }).compile();
 
-    controller = module.get<AccountsController>(AccountsController);
+    accountsController = module.get<AccountsController>(AccountsController);
+    accountsService = module.get<AccountsService>(AccountsService);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(accountsController).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should create an account', async () => {
+      const accountId = '12345';
+      const mockHttpStatus = HttpStatus.CREATED;
+
+      jest.spyOn(accountsService, 'createAccount').mockResolvedValue(mockHttpStatus);
+
+      const result = await accountsController.create(accountId);
+
+      expect(result).toEqual(mockHttpStatus);
+      expect(accountsService.createAccount).toHaveBeenCalledWith(accountId, true);
+    });
+  });
+
+  describe('findOne', () => {
+    it('should find an existing account', async () => {
+      // Arrange
+      const accountId = '12345';
+      const mockAccountResponse = {
+        accountId: '12345',
+        balance: 400,
+      };
+
+      jest.spyOn(accountsService, 'findOneByAccountId').mockResolvedValue(mockAccountResponse);
+
+      // Act
+      const result = await accountsController.findOne(accountId);
+
+      // Assert
+      expect(result).toEqual(mockAccountResponse);
+      expect(accountsService.findOneByAccountId).toHaveBeenCalledWith(accountId, false);
+    });
   });
 });
